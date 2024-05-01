4c27d1ff3796cf658bbf0f039a0fdd203c1dea30
collins <collinskazee456@gmail.com>
2024-05-01 13:23:18 +0300

    contact page link updated on profile.php

--- users_area/profile.php
+++ users_area/profile.php
@@ -60,7 +60,7 @@ session_start();
             <a class="nav-link" href="profile.php">My account<aria-hidden="true"></i> </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contact <i class="fa fa-phone" aria-hidden="true"></i></a>
+            <a class="nav-link" href="../contact.php">Contact <i class="fa fa-phone" aria-hidden="true"></i></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="../cart.php">Cart<i class="fa fa-shopping-cart" aria-hidden="true"></i><sup><?php cart_item(); ?></sup> </a>
